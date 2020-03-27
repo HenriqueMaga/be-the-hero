567afa7126870691994d5a20ce54bc8c9c363099
HenriqueMaga <henrique.ma400@gmail.com>
2020-03-27 17:48:52 -0300

    configurações de teste criadas

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,6 +13,17 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './scr/database/test.sqlite'
+    },
+    migrations:{
+      directory: './scr/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
   staging: {
     client: 'postgresql',
     connection: {
